5e40d65209c0d6ff84b60e16651603a71eb2f446
Kevin Hendrix <khendrix1994@hotmail.com>
2022-06-16 11:52:47 -0400

    Add functionality to torch on/off buttons

--- app/src/main/java/com/example/shakeflashlight/MainActivity.java
+++ app/src/main/java/com/example/shakeflashlight/MainActivity.java
@@ -2,13 +2,57 @@ package com.example.shakeflashlight;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button onButton;
+    Button offButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        onButton = findViewById(androidx.appcompat.R.id.on);
+        offButton = findViewById(androidx.appcompat.R.id.off);
+
+        onButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    torchOn();
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        offButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    torchOff();
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    private void torchOn() throws CameraAccessException {
+        CameraManager camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        String cameraId = camManager.getCameraIdList()[0];
+        camManager.setTorchMode(cameraId, true);
+    }
+    private void torchOff() throws CameraAccessException {
+        CameraManager camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        String cameraId = camManager.getCameraIdList()[0];
+        camManager.setTorchMode(cameraId, false);
     }
 }
